59370c4502a718ec65ca43715c445ec20b8ad723
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-05-20 21:01:43 +0700

    feat: ready to receive pressure and latitude data

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,11 +8,17 @@ export default function HomePage() {
   const [humidity, setHumidity] = useState(0);
   const [analog, setAnalog] = useState(0);
   const [temperature, setTemperature] = useState(0);
-  const [temperatures, setTemperatures] = useState([]);
+  const [temperatures, setTemperatures] = useState([0]);
   const [voltage, setVoltage] = useState(0);
-  const [voltages, setVoltages] = useState([]);
+  const [voltages, setVoltages] = useState([0]);
+  const [pressure, setPressure] = useState(0);
+  const [pressures, setPressures] = useState([0]);
+  const [altitude, setAltitude] = useState(0);
+  const [altitudes, setAltitudes] = useState([0]);
   const last10Temps = [];
   const last10Volts = [];
+  const last10Pressure = [];
+  const last10Altitude = [];
 
   useEffect(() => {
     const socket = new WebSocket(`ws://${EspIp}/ws`);
@@ -39,11 +45,31 @@ export default function HomePage() {
       setVoltage(data.voltage);
 
       setAnalog(data.analog);
+
+      if (last10Pressure.length > 10) {
+        last10Pressure.shift();
+      }
+      last10Pressure.push(data.pressure);
+      setPressures(last10Pressure);
+      setPressure(data.pressure);
+
+      if (last10Altitude.length > 10) {
+        last10Altitude.shift();
+      }
+      last10Altitude.push(data.altitude);
+      setAltitudes(last10Altitude);
+      setAltitude(data.altitude);
     };
   }, []);
   return (
-    <main className="flex flex-col justify-center items-center min-h-screen gap-10 py-20 bg-[#18161b]">
-      Humidity: {humidity}% Temperature: {temperature}°C
+    <main className="flex flex-col justify-center items-center min-h-screen gap-10 py-20 bg-white">
+      <h1>
+        Humidity: {humidity}% Temperature: {temperature}°C
+      </h1>
+      <h1>
+        Pressure: {pressure}% Altitude: {altitude}°C
+      </h1>
+      
       <div className="flex gap-10 items-center">
         <div>
           <Doughnut
@@ -54,7 +80,7 @@ export default function HomePage() {
                   label: "Humidity",
                   data: [humidity, 100 - humidity],
                   borderWidth: 1,
-                  backgroundColor: ["#0986DA", "#CA2FC8"]
+                  backgroundColor: ["#0986DA", "#CA2FC8"],
                 },
               ],
             }}
@@ -75,6 +101,7 @@ export default function HomePage() {
           />
         </div>
       </div>
+
       <div className="flex gap-10 items-center">
         <div>
           <Line
@@ -90,37 +117,40 @@ export default function HomePage() {
                 },
                 {
                   label: "Max Voltage",
-                  data: voltages.map(() => 3.30),
+                  data: voltages.map(() => 3.3),
                   borderWidth: 1,
                   borderColor: "#EE4493",
                   backgroundColor: "#CD136A",
-                }
+                },
               ],
             }}
           />
         </div>
         <div>
-          <Bar data={{
-            labels: ["Voltage"],
-            datasets: [
-              {
-                label: "Voltage",
-                data: [voltage],
-                borderWidth: 1,
-                borderColor: "#FAED30",
-                backgroundColor: "#F99C10",
-              },
-              {
-                label: "Max Voltage",
-                data: [3.30],
-                borderWidth: 1,
-                borderColor: "#EE4493",
-                backgroundColor: "#CD136A",
-              }
-            ],
-          }} />
+          <Bar
+            data={{
+              labels: ["Voltage"],
+              datasets: [
+                {
+                  label: "Voltage",
+                  data: [voltage],
+                  borderWidth: 1,
+                  borderColor: "#FAED30",
+                  backgroundColor: "#F99C10",
+                },
+                {
+                  label: "Max Voltage",
+                  data: [3.3],
+                  borderWidth: 1,
+                  borderColor: "#EE4493",
+                  backgroundColor: "#CD136A",
+                },
+              ],
+            }}
+          />
         </div>
       </div>
+
       <div className="flex gap-10 items-center">
         <div>
           <Doughnut
@@ -131,27 +161,65 @@ export default function HomePage() {
                   label: "Analog",
                   data: [analog, 4095 - analog],
                   borderWidth: 1,
+                  backgroundColor: ["#0986DA", "#CA2FC8"],
                 },
               ],
             }}
           />
         </div>
         <div>
-          <Bar data={{
-            labels: ["Analog"],
-            datasets: [
-              {
-                label: "Analog",
-                data: [analog],
-                borderWidth: 1,
-              },
-              {
-                label: "Max Analog",
-                data: [4095],
-                borderWidth: 1,
-              }
-            ],
-          }} />
+          <Bar
+            data={{
+              labels: ["Analog"],
+              datasets: [
+                {
+                  label: "Analog",
+                  data: [analog],
+                  borderWidth: 1,
+                },
+                {
+                  label: "Max Analog",
+                  data: [4095],
+                  borderWidth: 1,
+                },
+              ],
+            }}
+          />
+        </div>
+      </div>
+
+      <div className="flex gap-10 items-center">
+        <div>
+          <Line 
+            data={{
+              labels: pressures.map((_, index) => index),
+              datasets: [
+                {
+                  label: "Pressure",
+                  data: pressures,
+                  borderWidth: 1,
+                  borderColor: "#FAED30",
+                  backgroundColor: "#F99C10",
+                },
+              ],
+            }}  
+          />
+        </div>
+        <div>
+          <Line
+            data={{
+              labels: altitudes.map((_, index) => index),
+              datasets: [
+                {
+                  label: "Altitude",
+                  data: altitudes,
+                  borderWidth: 1,
+                  borderColor: "#FAED30",
+                  backgroundColor: "#F99C10",
+                },
+              ],
+            }}
+          />
         </div>
       </div>
     </main>
